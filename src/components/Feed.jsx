692aa03b3a775a50a13ebd5eae81f6cf7a619250
Rodrigo Matias De Souza <Rodrigo.MatiasDeSouza@coxautoinc.com>
2023-09-15 16:39:08 +1000

    Side bar functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import SideBar from './SideBar';
 
 
 export const Feed = () => {
@@ -8,7 +9,7 @@ export const Feed = () => {
     "column", md:"row"} }}>
       <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d',
        px: {sx: 0, md: 2 }}}>
-       SideBar
+       <SideBar />
       
       <Typography className="copyright"
         variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
