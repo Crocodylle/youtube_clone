09135d98c956310aa5a44ad0dd1b789662bf2820
Rodrigo Matias De Souza <Rodrigo.MatiasDeSouza@coxautoinc.com>
2023-09-15 15:04:37 +1000

    Initializing the repo

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'; 
+import { Paper, IconButton } from '@mui/material';
+import { SearchIcon } from '@mui/icons-material';
+
+export const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+export default SearchBar
